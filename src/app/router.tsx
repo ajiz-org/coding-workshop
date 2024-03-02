1f3bf5cf73363439548e394dfd151cdd96c29cd1
ahmed <ahmed>
2024-03-02 08:55:53 +0100

    add chat

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { pathname } from "./routes";
 import App from "./app";
+import Chat from "./chat";
 
 export default function () {
   return (
     <Routes>
       <Route path={pathname} element={<App />}></Route>
+      <Route path="chat" element={<Chat />}></Route>
       <Route path="*" element={<App />}></Route>
     </Routes>
   );
